be49824f69fae07ff997330cba3d82de1036ccbe
eXpressionist <expressioned@gmail.com>
2023-08-18 18:37:41 +0300

    Update and rename multisplit(re).py to regular_funcs.py

--- multisplit(re).py
+++
@@ -1,6 +0,0 @@
-import re
-'''Функция принимает строку с разделителями и массив из разделителей, выдает слова без разделителей'''
-
-def multiple_split(string,delimeters):
-    regex = re.compile(f'{"|".join(map(re.escape, delimeters))}')
-    return re.split(regex, string)
---
+++ regular_funcs.py
@@ -0,0 +1,41 @@
+import re
+'''Функция принимает строку с разделителями и массив из разделителей, выдает слова без разделителей'''
+def multiple_split(string,delimeters):
+    regex = re.compile(f'{"|".join(map(re.escape, delimeters))}')
+    return re.split(regex, string)
+
+'''Функция удаляет лишние пробелы в строке'''
+def normalize_whitespace(string):
+    return re.sub(r'\s+', ' ', string)
+
+'''Функция печатает все ссылки и их описания (html)'''
+def html_href(text):
+    pattern = r'<a href="(.+)">(.+)</a>'
+
+    for address, pointer in re.findall(pattern, text):
+        print(f'{address}, {pointer}')
+
+'''Функция находит теги и их атрибуты (html)'''
+def find_html_tags(data):
+    # Регулярное выражение для поиска тегов и их атрибутов
+    tag_pattern = re.compile(r'<(\w+)(\s+[^>]*)?>')
+    attr_pattern = re.compile(r'(\S+)="[^"]*"')
+
+    tags = {}
+
+    # Поиск всех тегов и их атрибутов
+    for match in tag_pattern.finditer(data):
+        tag_name = match.group(1)
+        attrs = match.group(2)
+        if tag_name not in tags:
+            tags[tag_name] = set()
+        if attrs:
+            attrs_list = [m.group(1) for m in attr_pattern.finditer(attrs)]
+            tags[tag_name].update(attrs_list)
+
+    # Сортировка тегов и их атрибутов
+    for tag, attrs in sorted(tags.items()):
+        if attrs:
+            print(f'{tag}: {", ".join(sorted(attrs))}')
+        else:
+            print(f'{tag}:')
